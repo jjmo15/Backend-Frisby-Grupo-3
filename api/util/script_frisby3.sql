9d2a08b0a058577cb4c12c65db655bfde4951d60
josemolina1598@gmail.com <josemolina1598@gmail.com>
2020-10-30 16:57:38 -0500

    Adjunto Script Bd

--- api/util/script_frisby3.sql
+++ api/util/script_frisby3.sql
@@ -87,3 +87,9 @@ CREATE TABLE public.evaluacion_usuarios (
 ALTER TABLE public.aplicar_ofertas ADD CONSTRAINT aplicar_oferta_usuario_fk FOREIGN KEY (usuario) REFERENCES usuarios(identificacion) ON UPDATE CASCADE;
 ALTER TABLE public.aplicar_ofertas ADD CONSTRAINT aplicar_oferta_oferta_fk FOREIGN KEY (oferta) REFERENCES ofertas(id) ON UPDATE CASCADE;
 ALTER TABLE public.empleados ADD CONSTRAINT empleado_usuario_fk FOREIGN KEY (identificacion) REFERENCES usuarios(identificacion) ON UPDATE CASCADE;
+ALTER TABLE public.ofertas ADD CONSTRAINT oferta_cargo_fk FOREIGN KEY (cargo) REFERENCES cargos(id) ON UPDATE CASCADE;
+ALTER TABLE public.empleados ADD CONSTRAINT empleado_cargo_fk FOREIGN KEY (cargo) REFERENCES cargos(id) ON UPDATE CASCADE;
+ALTER TABLE public.capacitacion_empleados ADD CONSTRAINT capacitacion_empleado_empleado_fk FOREIGN KEY (empleado) REFERENCES empleados(id) ON UPDATE CASCADE;
+ALTER TABLE public.capacitacion_empleados ADD CONSTRAINT capacitacion_empleado_capacitacion_fk FOREIGN KEY (capacitacion) REFERENCES capacitaciones(id) ON UPDATE CASCADE;
+ALTER TABLE public.evaluacion_usuarios ADD CONSTRAINT evaluacion_usuario_evaluacion_fk FOREIGN KEY (evaluacion) REFERENCES evaluaciones(id) ON UPDATE CASCADE;
+ALTER TABLE public.evaluacion_usuarios ADD CONSTRAINT evaluacion_usuario_usuario_fk FOREIGN KEY (usuario) REFERENCES usuarios(identificacion) ON UPDATE CASCADE;
